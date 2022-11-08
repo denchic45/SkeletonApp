f1361ac696b24b8470fdbdd7ad0091f5e8c6a190
denchic45 <denchic860@gmail.com>
2022-11-08 11:41:39 +0300

    Update typography

--- app/src/main/kotlin/com/denchic45/skeletonapp/ui/theme/Typogrpahy.kt
+++ app/src/main/kotlin/com/denchic45/skeletonapp/ui/theme/Typogrpahy.kt
@@ -1,34 +1,91 @@
 package com.denchic45.skeletonapp.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import com.denchic45.skeletonapp.R
 
-// Set of Material typography styles to start with
+val RobotoFamily = FontFamily(
+    Font(R.font.roboto, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold)
+)
+
+val GilroyFamily = FontFamily(
+    Font(R.font.gilroy_regular, FontWeight.Normal),
+    Font(R.font.gilroy_medium, FontWeight.Medium),
+    Font(R.font.gilroy_semibold, FontWeight.SemiBold),
+    Font(R.font.gilroy_bold, FontWeight.Bold)
+)
+
+val defaultTypography = Typography()
+
+@OptIn(ExperimentalUnitApi::class)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = defaultTypography.displayLarge.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.Medium
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayMedium = defaultTypography.displayMedium.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.Normal
+    ),
+    displaySmall = defaultTypography.displaySmall.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.Normal
+    ),
+    headlineLarge = defaultTypography.headlineLarge.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.Medium
+    ),
+    headlineMedium = defaultTypography.headlineMedium.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.Medium
+    ),
+    headlineSmall = defaultTypography.headlineSmall.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.Normal
+    ),
+    titleLarge = defaultTypography.titleLarge.copy(
+        fontFamily = GilroyFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = TextUnit(0f, TextUnitType.Em)
+    ),
+    titleMedium = defaultTypography.titleMedium.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    titleSmall = defaultTypography.titleSmall.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    bodyLarge = defaultTypography.bodyLarge.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.Medium
+    ),
+    bodyMedium = defaultTypography.bodyMedium.copy(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Normal
+    ),
+    bodySmall = defaultTypography.bodySmall.copy(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Normal
+    ),
+    labelLarge = defaultTypography.labelLarge.copy(
+        fontFamily = GilroyFamily,
+        fontWeight = FontWeight.SemiBold
+    ),
+    labelMedium = defaultTypography.labelMedium.copy(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Medium
+    ),
+    labelSmall = defaultTypography.labelSmall.copy(
+        fontFamily = RobotoFamily,
+        fontWeight = FontWeight.Medium
+    ),
 )
